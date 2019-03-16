b291a9ae7c815684075ac7f6d1e53670f782a3e2
jittythekitty <jittythekitty@gmail.com>
2019-03-16 13:26:50 -0400

    updated user page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./Nav.scss";
 import Navbar from 'react-bootstrap/Navbar';
+import Logo from '../../../src/logo.svg';
 
   function Nav(props) {
     return (
 
-      <Navbar bg="light">
-      <Navbar.Brand href="#home">Bee The Change</Navbar.Brand>
+      <Navbar className="navbarStyle">
+      <Navbar.Brand className="navheader" href="#home">     
       <img
-        src={props.children}
-        width="30"
-        height="30"
+        src={Logo}
+        width="55"
+        height="55"
         className="d-inline-block align-top"
-        alt="React Bootstrap logo"
+        alt="Bee The Change Logo"
       />
+      <span className='textheader'>
+      {' Bee The Change '}
+      </span>
+      </Navbar.Brand>
+  
   </Navbar>
     );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,72 +1,72 @@
 
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { withAuth } from '@okta/okta-react';
+// import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import { withAuth } from '@okta/okta-react';
 
-withAuth(class Home extends Component {
-    state = { authenticated: null };
+// withAuth(class Home extends Component {
+//     state = { authenticated: null };
 
-    checkAuthentication = async() => {
-        const authenticated = await this.props.auth.isAuthenticated();
-        if (authenticated !== this.state.authenticated) {
-            this.setState({ authenticated });
-        }
-    }
+//     checkAuthentication = async() => {
+//         const authenticated = await this.props.auth.isAuthenticated();
+//         if (authenticated !== this.state.authenticated) {
+//             this.setState({ authenticated });
+//         }
+//     }
 
-    async componentDidMount() {
-        this.checkAuthentication();
-    }
+//     async componentDidMount() {
+//         this.checkAuthentication();
+//     }
 
-    async componentDidUpdate() {
-        this.checkAuthentication();
-    }
+//     async componentDidUpdate() {
+//         this.checkAuthentication();
+//     }
 
-    login = async() => {
-        this.props.auth.login('/');
-    }
+//     login = async() => {
+//         this.props.auth.login('/');
+//     }
 
-    logout= async() => {
-        this.props.auth.logout('/');
-    }
+//     logout= async() => {
+//         this.props.auth.logout('/');
+//     }
 
-    render() {
-        if (this.state.authenticated === null) return null;
+//     render() {
+//         if (this.state.authenticated === null) return null;
 
-        const button = this.state.authenticated ?
-            <button onClick={this.logout}>Logout</button> :
-            <button onClick={this.login}>Login</button>;
+//         const button = this.state.authenticated ?
+//             <button onClick={this.logout}>Logout</button> :
+//             <button onClick={this.login}>Login</button>;
 
-        return (
-            <div>
-                {button}
-            </div>
-        );
-    }
-});
+//         return (
+//             <div>
+//                 {button}
+//             </div>
+//         );
+//     }
+// });
 
-export default withAuth;
-
-
-import React, { Component } from "react";
-import FrontNav from "../components/FrontNav";
-import Slider from "../components/Slider/Slider";
-import SectionA from "../components/SectionA";
-import Footer from "../components/Footer";
-import Wrapper from "../components/Wrapper";
+// export default withAuth;
 
 
-class Home extends Component {
-    render() {
-        return (
-            <Wrapper>
-                <FrontNav />
-                <Slider />
-                {/* <SectionA  /> */}
-                {/* <Footer />  */}
-            </Wrapper>
-        )
-    };
-    };
-    export default Home;
+// import React, { Component } from "react";
+// import FrontNav from "../components/FrontNav";
+// import Slider from "../components/Slider/Slider";
+// import SectionA from "../components/SectionA";
+// import Footer from "../components/Footer";
+// import Wrapper from "../components/Wrapper";
+
+
+// class Home extends Component {
+//     render() {
+//         return (
+//             <Wrapper>
+//                 <FrontNav />
+//                 <Slider />
+//                 {/* <SectionA  /> */}
+//                 {/* <Footer />  */}
+//             </Wrapper>
+//         )
+//     };
+//     };
+//     export default Home;
     
 
