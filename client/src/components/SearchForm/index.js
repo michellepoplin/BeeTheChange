1a791262d89415fc1aeb8a6f5f743c562b40d31a
Krista Brehm <kristabrehm1@gmail.com>
2019-03-20 18:27:24 -0400

    api

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -6,20 +6,20 @@ function SearchForm(props) {
   return (
     <form className="search">
       <div className="form-group">
-        <label htmlFor="farm">farm Name:</label>
+        <label htmlFor="farm">Farmer Name:</label>
         <input
           value={props.search}
           onChange={props.handleInputChange}
-          name="farm"
+          name="farmers"
           list="Farmers"
           type="text"
           className="form-control"
           placeholder="Type a Farmer in to begin"
-          id="Farm"
+          id="Farmers"
         />
         <datalist id="farmers">
-          {props.farms.map(farm => (
-            <option value={farm} key={farm} />
+          {props.farmerss.map(farmers => (
+            <option value={farmers} key={farmers} />
           ))}
         </datalist>
         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
