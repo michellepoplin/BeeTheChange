b8c9c6c22200e254849bdd3c9f7f6f3508828bf1
Allie Valder <avalder19@gmail.com>
2019-03-20 16:43:23 -0400

    the right code for Main.js

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,13 +1,17 @@
 
 import React, { Component } from "react";
 import FrontNav from "../components/FrontNav";
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import {Row, Col } from 'react-flexbox-grid';
 import Slider from "../components/Slider/Slider";
-// import SectionA from "../components/SectionA";
-// import Footer from "../components/Footer";
+import SectionA from "../components/SectionA";
+import SectionB from "../components/SectionB";
+import SectionC from "../components/SectionC";
+import Footer from "../components/Footer";
 import Wrapper from "../components/Wrapper";
 // import Container from "react-bootstrap/Container";
 import Section from '../components/Section';
+import { Container } from "mdbreact";
+
 
 
 class Main extends Component {
@@ -15,27 +19,44 @@ class Main extends Component {
         return (
             <Wrapper>
               <FrontNav />
-                <Grid fluid>
                   <Section>
                     <Row>
                       <Col xs={12}>
-                         {/* <Container> */}
                              <Slider />
-                         {/* </Container> */}
-                       </Col>
+                      </Col>
                     </Row>
-                    </Section>
-                </Grid>
+                  </Section>
+                  <br />
+                  <Section>
+                    <Container>
+                      <Row>
+                        <Col xs={12}>
+                             <SectionA />
+                        </Col>
+                     </Row>
+                    </Container>
+                  </Section>
+                  <br />
+                  <Section>
+                      <Row>
+                        <Col xs={12}>
+                             <SectionB />
+                        </Col>
+                     </Row>
+                  </Section>
+                  <br />
+                  <Section>
+                    <Container>
+                      <Row>
+                        <Col xs={12}>
+                             <SectionC />
+                        </Col>
+                     </Row>
+                    </Container>
+                  </Section>
+                <Footer />
             </Wrapper>
         )
     };
     };
     export default Main;
-    
-
-{/* <Wrapper> */}
-     {/* <FrontNav /> */}
-     {/* <Slider /> */}
-    {/* <SectionA  /> */}
-     {/* <Footer />  */}
-{/* </Wrapper> */}
