09507b778a672c283f231f9aa05131dd145b5f2f
Krista Brehm <kristabrehm1@gmail.com>
2019-03-19 21:01:17 -0400

    fixing merge issues

--- client/src/components/Usercard/index.js
+++ client/src/components/Usercard/index.js
@@ -1,25 +1,29 @@
 import React from "react";
-import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import './Usercard.scss';
+import { Row, Col } from 'react-bootstrap';
 import ProfilePicModal from '../../components/Modals/ProfilePicModal/';
 
 
-class Usercard extends React.Component {
-  render() {
-    return (
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="https://via.placeholder.com/100" />
-        <ProfilePicModal />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of
-            the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    );
-  }
 
+
+function Usercard(props) {
+  return (
+
+    <Container className={"profilePicture"}>
+      <Col md={6}></Col>
+      <Col md={6}>
+        <Row>
+          <Image src="https://via.placeholder.com/250" roundedCircle />
+          <ProfilePicModal/>
+        </Row>
+        <Row><h3 className={"userHeader"}>Bob The Farmer</h3></Row>
+      </Col>
+    </Container>
+
+
+  );
 }
 
 export default Usercard;
