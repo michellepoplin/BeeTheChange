0e544584b8eafdf42b27dd5d948bc4bebed42716
Krista Brehm <kristabrehm1@gmail.com>
2019-03-21 16:55:26 -0400

    authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Farmer from "./pages/User";
+// import Main from "./pages/Main";
 import Search from "./pages/Search";
 import Settings from "./pages/Settings";
 import Devpage from './pages/Devpage';
 import Home from "./pages/Home";
+// import Auth from './Auth/Auth.js';
+// import { Button } from 'react-bootstrap';
 
+class App extends Component {
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
 
-function App() {
-  return (
-    <Router>
-      <Switch>
+  login() {
+    this.props.auth.login();
+  }
+
+  logout() {
+    this.props.auth.logout();
+  }
+
+  componentDidMount() {
+    const { renewSession } = this.props.auth;
+
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      renewSession();
+    }
+  }
+
+  render() {
+    // const { isAuthenticated } = this.props.auth;
+
+    // <Button bsStyle="primary" className="btn-margin" onClick={this.goTo.bind(this, 'home')}>Home</Button>
+    // {!isAuthenticated() && (<Button bsStyle="primary" className="btn-margin" onClick={this.login.bind(this)}>Log In</Button>)}
+    // { isAuthenticated() && (<Button bsStyle="primary" className="btn-margin" onClick={this.logout.bind(this)}>Log Out</Button>)}
+
+    return (
+      <Router>
+        <Switch>
+          {/* <Route exact path="/" component={Main} /> */}
           <Route path="/" exact={true} component={Home} />
           <Route exact path="/farmer/:id" component={Farmer} />
           <Route exact path="/search" component={Search} />
           {/* TODO: Update Settings To Not Require User Id Param After Authentication Is Implemented */}
           <Route exact path="/settings/:id" component={Settings} />
           <Route exact path="/developers" component={Devpage} />
-          <Route exact path="/farmersearch" component={Farmersearch} />
-        </Switch> 
-      </div>
-    </Router>
-  );
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
 import FrontNav from "../components/FrontNav";
-import {Row, Col } from 'react-flexbox-grid';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import Slider from "../components/Slider/Slider";
 import SectionA from "../components/SectionA";
 import SectionB from "../components/SectionB";
@@ -12,51 +12,80 @@ import Wrapper from "../components/Wrapper";
 import Section from '../components/Section';
 import { Container } from "mdbreact";
 
+// NOT SURE WHICH VERSION OF MAIN IS CURRECT
 
+// class Main extends Component {
+//   render() {
+//     return (
+//       <Wrapper>
+//         <FrontNav />
+//         <Grid fluid>
+//           <Section>
+//             <Row>
+//               <Col xs={12}>
+//                 {/* <Container> */}
+//                   <Slider />
+//                 {/* </Container> */}
+//               </Col>
+//             </Row>
+//           </Section>
+//         </Grid>
+//       </Wrapper>
+//     );
+//   }
+// }
 
 class Main extends Component {
-    render() {
-        return (
-            <Wrapper>
-              <FrontNav />
-                  <Section>
-                    <Row>
-                      <Col xs={12}>
-                             <Slider />
-                      </Col>
-                    </Row>
-                  </Section>
-                  <br />
-                  <Section>
-                    <Container>
-                      <Row>
-                        <Col xs={12}>
-                             <SectionA />
-                        </Col>
-                     </Row>
-                    </Container>
-                  </Section>
-                  <br />
-                  <Section>
-                      <Row>
-                        <Col xs={12}>
-                             <SectionB />
-                        </Col>
-                     </Row>
-                  </Section>
-                  <br />
-                  <Section>
-                    <Container>
-                      <Row>
-                        <Col xs={12}>
-                             <SectionC />
-                        </Col>
-                     </Row>
-                    </Container>
-                  </Section>
-                <Footer />
-            </Wrapper>
-        )
-    };
-    };
-    export default Main;
+  render() {
+    return (
+      <Wrapper>
+        <FrontNav />
+        <Section>
+          <Row>
+            <Col xs={12}>
+              <Slider />
+            </Col>
+          </Row>
+        </Section>
+        <br />
+        <Section>
+          <Container>
+            <Row>
+              <Col xs={12}>
+                <SectionA />
+              </Col>
+            </Row>
+          </Container>
+        </Section>
+        <br />
+        <Section>
+            <Row>
+              <Col xs={12}>
+                <SectionB />
+              </Col>
+            </Row>
+        </Section>
+        <br />
+        <Section>
+          <Container>
+            <Row>
+              <Col xs={12}>
+                <SectionC />
+              </Col>
+            </Row>
+          </Container>
+        </Section>
+        <Footer />
+      </Wrapper>
+    )
+  };
+};
+
+export default Main;
+
+{/* <Wrapper> */}
+  {/* <FrontNav /> */}
+  {/* <Slider /> */}
+  {/* <SectionA /> */}
+  {/* <Footer /> */}
+{/* </Wrapper> */}
