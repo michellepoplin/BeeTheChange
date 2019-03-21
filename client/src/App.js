7cb8b0952892bd76b35d6632a72a7c295c10e064
jittythekitty <jittythekitty@gmail.com>
2019-03-21 12:10:26 -0400

    let me merge

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,63 @@ import Main from "./pages/Main";
 import Devpage from "./pages/Devpage";
 import Farmersearch from "./pages/Farmersearch";
 import './App.scss';
+import Auth from './Auth/Auth.js';
+import { Navbar, Button } from 'react-bootstrap';
 
-function App() {
+class App extends Component {
+goTo(route) {
+  this.props.history.replace(`/${route}`)
+}
+
+login() {
+  this.props.auth.login();
+}
+
+logout() {
+  this.props.auth.logout();
+}
+
+componentDidMount() {
+  const { renewSession } = this.props.auth;
+
+  if (localStorage.getItem('isLoggedIn') === 'true') {
+    renewSession();
+  }
+}
+
+render() {
+  const { isAuthenticated } = this.props.auth;
+
   return (
     <Router>
       <div>
+      <Button
+              bsStyle="primary"
+              className="btn-margin"
+              onClick={this.goTo.bind(this, 'home')}
+            >
+              Home
+            </Button>
+            {
+              !isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.login.bind(this)}
+                  >
+                    Log In
+                  </Button>
+                )
+            }
+            {
+              isAuthenticated() && (
+                  <Button
+                    bsStyle="primary"
+                    className="btn-margin"
+                    onClick={this.logout.bind(this)}
+                  >
+                    Log Out
+                  </Button>
         <Switch>
           <Route exact path="/developers" component={Devpage} />
           <Route exact path="/" component={Main} />
